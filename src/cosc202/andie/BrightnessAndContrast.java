137f550a8a67e547fd4fb48407b32f33a1140d52
Jake Norton <norja159@student.otago.ac.nz>
2022-03-22 16:12:13 +1300

    Added SoftBlur and basic version of brightness and contrast

---
+++ src/cosc202/andie/BrightnessAndContrast.java
@@ -0,0 +1,39 @@
+package cosc202.andie;
+
+import java.awt.image.*;
+
+/**
+ * Image Operation to change the brightness and contrast of given image.
+ */
+
+public class BrightnessAndContrast implements ImageOperation, java.io.Serializable {
+    private static double contrast = 0;
+    private static double brightness = 10;
+    
+    public BrightnessAndContrast(){}
+
+    public BufferedImage apply(BufferedImage input){
+        for (int y = 0; y < input.getHeight(); ++y) {
+            for (int x = 0; x < input.getWidth(); ++x) {
+                int argb = input.getRGB(x, y);
+                int a = (argb & 0xFF000000) >> 24;
+                int r = (argb & 0x00FF0000) >> 16;
+                int g = (argb & 0x0000FF00) >> 8;
+                int b = (argb & 0x000000FF);
+
+                argb = (a << 24) | (formula(r) << 16) | (formula(g) << 8) | formula(b);
+                input.setRGB(x, y, argb);
+            }
+        }
+
+        return input;
+    }
+    //Method to calculate the changed value of the pixel
+    public int formula(int v){
+        int output = (int)((1.0 + contrast/100.0)*(v - 127.5) + (127.5*(1.0 + (brightness/100))));
+        if (output > 255){
+            output = 255;
+        }
+        return output;
+    }
+}
--- src/cosc202/andie/FilterActions.java
+++ src/cosc202/andie/FilterActions.java
@@ -35,6 +35,7 @@ public class FilterActions {
     public FilterActions() {
         actions = new ArrayList<Action>();
         actions.add(new MeanFilterAction("Mean filter", null, "Apply a mean filter", Integer.valueOf(KeyEvent.VK_M)));
+        actions.add(new SoftBlurAction("Soft blur", null, "Apply a soft blur", Integer.valueOf(KeyEvent.VK_B)));
     }
 
     /**
@@ -112,5 +113,18 @@ public class FilterActions {
             target.getParent().revalidate();
         }
 
+    }
+    public class SoftBlurAction extends ImageAction{
+
+        SoftBlurAction(String name, ImageIcon icon, 
+                        String desc, Integer mnemonic){
+            super(name, icon, desc, mnemonic);
+        }
+        
+        public void actionPerformed(ActionEvent e){
+            target.getImage().apply(new SoftBlur());
+            target.repaint();
+            target.getParent().revalidate();
+        }
     }
 }
