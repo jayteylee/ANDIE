7797e6d6088b94dddb46755046d9fe608f35eb47
leeda371 <leeda371@student.otago.ac.nz>
2022-04-07 12:47:53 +1200

    Adding Javadoc and code comments.

--- src/cosc202/andie/FlipImageHorizontal.java
+++ src/cosc202/andie/FlipImageHorizontal.java
@@ -2,6 +2,12 @@ package cosc202.andie;
 
 import java.awt.image.BufferedImage;
 
+/**
+ * <p> An action that unables the image to be flip horizotally.</p>
+ * 
+ * @author Daniel Lee
+ */
+
 public class FlipImageHorizontal implements ImageOperation, java.io.Serializable {
     public BufferedImage apply(BufferedImage input) {
         int width = input.getWidth();
@@ -10,6 +16,7 @@ public class FlipImageHorizontal implements ImageOperation, java.io.Serializable
 
         BufferedImage rotatedImage = new BufferedImage(height, width, imageType);
 
+        //change the value of x to achieve the horizontal flip
         for(int y = 0; y < height; y++) {
             for(int x = 0; x < width; x++) {
                 rotatedImage.setRGB((width - 1) - x, y, input.getRGB(x, y));
--- src/cosc202/andie/FlipImageVertical.java
+++ src/cosc202/andie/FlipImageVertical.java
@@ -2,6 +2,11 @@ package cosc202.andie;
 
 import java.awt.image.BufferedImage;
 
+/**
+ * <p> An action that unables the image to be flip vertically.</p>
+ * 
+ * @author Daniel Lee
+ */
 public class FlipImageVertical implements ImageOperation, java.io.Serializable {
     public BufferedImage apply(BufferedImage input) {
         int width = input.getWidth();
@@ -10,6 +15,7 @@ public class FlipImageVertical implements ImageOperation, java.io.Serializable {
 
         BufferedImage rotatedImage = new BufferedImage(height, width, imageType);
 
+        //change the value of y to achieve the vertical flip.
         for(int y = 0; y < height; y++) {
             for(int x = 0; x < width; x++) {
                 rotatedImage.setRGB(x,(height - 1) - y, input.getRGB(x, y));
