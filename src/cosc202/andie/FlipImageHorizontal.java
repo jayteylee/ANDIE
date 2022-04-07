bbdbb05d9f2297508d50ed6516eda4f79babcc44
leeda371 <leeda371@student.otago.ac.nz>
2022-04-08 09:16:42 +1200

    Patch Flip image. Now image can be flip horizontally and vertically regardless of size.

--- src/cosc202/andie/FlipImageHorizontal.java
+++ src/cosc202/andie/FlipImageHorizontal.java
@@ -14,16 +14,16 @@ public class FlipImageHorizontal implements ImageOperation, java.io.Serializable
         int height = input.getHeight();
         int imageType = input.getType();
 
-        BufferedImage rotatedImage = new BufferedImage(height, width, imageType);
+        BufferedImage flipImage = new BufferedImage(width, height, imageType);
 
         //change the value of x to achieve the horizontal flip
         for(int y = 0; y < height; y++) {
             for(int x = 0; x < width; x++) {
-                rotatedImage.setRGB((width - 1) - x, y, input.getRGB(x, y));
+                flipImage.setRGB((width - 1) - x, y, input.getRGB(x, y));
             }
         }
         
-        return rotatedImage;
+        return flipImage;
     }
 
     
