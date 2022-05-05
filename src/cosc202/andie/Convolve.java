a3cf94acc5541513801ec27ab547498f3f3715a1
magicalmanjack <magicalmanjack@gmail.com>
2022-05-06 10:36:30 +1200

    Added comments to Convolve.java

--- src/cosc202/andie/Convolve.java
+++ src/cosc202/andie/Convolve.java
@@ -2,27 +2,53 @@ package cosc202.andie;
 
 import java.awt.image.*;
 
+/**
+ * <p> A custom implementation of image convolution.</p>
+ * 
+ * <p> Convolve is similiar to the java ConvolveOp class except the edges of an image are handled differently.
+ * If a pixel to sample is outside of the image then the nearest pixel is used.</p> 
+ * 
+ * @author Jack Bredenbeck.
+ */
 public class Convolve {
     private Kernel kernel;
+
+    /**<p>Constructs a new convolve object.</p>*/
     public Convolve(Kernel k) {
         kernel = k;
     }
 
+    /**<p>Splits a color into different channels.</p>
+     * 
+     * <p>An integer color value is split into its different color channels and then each channel is placed into an array.</p> 
+     * 
+     * @param local The color value to be split.
+     * @return An array of color channels.
+     * */
     private int[] extractColors(int local) {
         int[] colors = {(local & 0xFF000000) >> 24,
         (local & 0x00FF0000) >> 16, (local & 0x0000FF00) >> 8, (local & 0x000000FF)};
         return colors;
     }
 
+    /**<p>Combines color channels into one value.</p>
+     * 
+     * <p>Each color is recombined back into one integer value. If the color is outside the region 0-255
+     * it is then clipped.</p>
+     * @param colors The array of color channels to be recombined.
+     * @return The one int value describing all of the color channels.
+     */
     private int combineColors(int[] colors) {
         int finalColor = 0;
         for(int i = 0; i < colors.length; i++) {
             finalColor = finalColor << 8;
             // If there is an offset then add it to colors[i] here.
             if(colors[i] > 255) {
+                //Clipping to large values.
                 colors[i] = 255;
             }
             if(colors[i] < 0) {
+                //Clipping to small values.
                 colors[i] = 0;
             }
             finalColor = finalColor | colors[i];
@@ -30,6 +56,10 @@ public class Convolve {
         return finalColor;
     }
 
+    /**<p>Converts a int array to float array.</p>
+     * @param in The array to be converted.
+     * @return The converted array.
+    */
     private float[] intsToFloats(int[] in) {
         float[] out = new float[in.length];
         for(int i = 0; i < in.length; i++) {
@@ -38,6 +68,10 @@ public class Convolve {
         return out;
     }
 
+    /**<p>Converts a float array to int array.</p>
+     * @param in The array to be converted.
+     * @return The converted array.
+    */
     private int[] floatsToInts(float[] in) {
         int[] out = new int[in.length];
         for(int i = 0; i < in.length; i++) {
@@ -46,6 +80,14 @@ public class Convolve {
         return out;
     }
 
+    /**<p>Applies a convolution filter to an image.</p>
+     * <p>The input image is used as the source for the convolution and the result of the convolution
+     * is put in the output image. If pixels are sampled that are outside of the bounds of the image then the nearest
+     * pixels are used instead.</p>
+     * 
+     * @param in The source image of the convolution.
+     * @param out The destination image for the result of the convolution.
+     */
     public void filter(BufferedImage in, BufferedImage out) {
         for(int x = 0; x < in.getWidth(); x++) {
             for(int y = 0; y < in.getHeight(); y++) {
